b5f5ccea99f658b523e9cbd0a0bb9d484e12917d
Kim Eui-Gyom <deholexp@gmail.com>
2014-07-11 23:38:45 +0900

    Bug fix for sample codes

--- DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.m
+++ DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.m
@@ -26,15 +26,17 @@
     
     slideUpMenu = [[DESlideUpMenuViewController alloc] initWithFrame:CGRectMake(0, 0, self.targetView.frame.size.width, self.targetView.frame.size.height)];
     
+    [slideUpMenu setDelegate:self];
     [slideUpMenu setMainButtonSize:CGSizeMake(50.0f, 50.0f)];
     [slideUpMenu setButtonCorner:DESlideButtonCornerBottomRight];
     [slideUpMenu setButtonSide:DESlideButtonSideRow];
     
+    [slideUpMenu setMainButtonWithImage:nil text:nil];
     [slideUpMenu addSideButtonWithImage:nil text:nil];
     [slideUpMenu addSideButtonWithImage:nil text:nil];
     [slideUpMenu addSideButtonWithImage:nil text:nil];
     
-    [slideUpMenu.view addSubview:self.targetView];
+    [self.targetView addSubview:slideUpMenu.view];
 }
 
 - (IBAction)touchHighlightButton:(id)sender
