77a8376322e1a7d501723a802a72ab53a4510389
Kim Eui-Gyom <dehol@me.com>
2014-07-11 11:59:20 +0900

    Generate example code

--- DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.h
+++ DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DESlideUpMenuViewController.h"
 
-@interface DEViewController : UIViewController
+@interface DEViewController : UIViewController <DESlideUpMenuDelegate>
 
 @end
--- DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.m
+++ DESlideUpMenuExample/DESlideUpMenuExample/DEViewController.m
@@ -9,6 +9,12 @@
 #import "DEViewController.h"
 
 @interface DEViewController ()
+{
+    DESlideUpMenuViewController *slideUpMenu;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *targetView;
+@property (weak, nonatomic) IBOutlet UILabel *indexLabel;
 
 @end
 
@@ -17,13 +23,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    slideUpMenu = [[DESlideUpMenuViewController alloc] initWithFrame:CGRectMake(0, 0, self.targetView.frame.size.width, self.targetView.frame.size.height)];
+    
+    [slideUpMenu setMainButtonSize:CGSizeMake(50.0f, 50.0f)];
+    [slideUpMenu setButtonCorner:DESlideButtonCornerBottomRight];
+    [slideUpMenu setButtonSide:DESlideButtonSideRow];
+    
+    [slideUpMenu addSideButtonWithImage:nil text:nil];
+    [slideUpMenu addSideButtonWithImage:nil text:nil];
+    [slideUpMenu addSideButtonWithImage:nil text:nil];
+    
+    [slideUpMenu.view addSubview:self.targetView];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)touchHighlightButton:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [slideUpMenu highlightButtonIndex:0];
+}
+
+#pragma mark - DESlideUpMenu Delegate
+
+- (void)touchedSlideUpMenuButton:(UIButton*)button index:(NSInteger)buttonIndex
+{
+    [self.indexLabel setText:[NSString stringWithFormat:@"Selected Index : %tu", buttonIndex]];
 }
 
 @end
