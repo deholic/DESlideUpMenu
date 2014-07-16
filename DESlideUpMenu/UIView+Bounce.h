f63409ea9296c7cfae7c8e674a267ffad86678f2
Kim Eui-Gyom <deholexp@gmail.com>
2014-07-16 10:55:44 +0900

    add sample images

---
+++ DESlideUpMenu/UIView+Bounce.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Bounce.h
+//  DESlideUpMenuExample
+//
+//  Created by Kim Eui-Gyom on 2014. 7. 14..
+//  Copyright (c) 2014년 deStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Bounce)
+
+@end
---
+++ DESlideUpMenu/UIView+Bounce.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Bounce.m
+//  DESlideUpMenuExample
+//
+//  Created by Kim Eui-Gyom on 2014. 7. 14..
+//  Copyright (c) 2014년 deStudio. All rights reserved.
+//
+
+#import "UIView+Bounce.h"
+
+@implementation UIView (Bounce)
+
+@end
